bcb4373bf40aa1e76bf04c0e532a1161e5899170
caokanglong <kanglong.cao@huofeng.cn>
2022-11-17 15:29:18 +0800

    新增uview ui

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
+import Vue from 'vue'
 import App from './App'
+// vuex
 import store from '@/store'
 
-// 自定义插件
+// uview ui
+import uView from '@/uni_modules/uview-ui'
+Vue.use(uView)
+// 配置uview ui默认值
+uni.$u.config.unit = 'rpx'
 
-
-import Vue from 'vue'
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
