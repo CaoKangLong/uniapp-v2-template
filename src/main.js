03ffcfdb59d61695b5004c96382d2c30a63ed628
caokanglong <kanglong.cao@huofeng.cn>
2022-11-17 15:52:18 +0800

    add 说明文件

--- src/main.js
+++ src/main.js
@@ -3,7 +3,17 @@ import App from './App'
 // vuex
 import store from '@/store'
 
-// 引入uView 
+// 多语言 需要则配置
+// import VueI18n from 'vue-i18n'
+// import messages from '@/locale/index'
+// let i18nConfig = {
+//   locale: uni.getLocale(), // 获取已设置的语言
+//   messages
+// }
+// Vue.use(VueI18n)
+// const i18n = new VueI18n(i18nConfig);
+
+// uView ui 
 import uView from '@/uni_modules/uview-ui'
 Vue.use(uView)
 uni.$u.config.unit = 'rpx'
