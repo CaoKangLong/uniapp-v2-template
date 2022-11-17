746825df774e3944fc51e6bf6de9d94ecc05607d
caokanglong <kanglong.cao@huofeng.cn>
2022-11-17 16:15:32 +0800

    store 仓库初始化

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,46 +1,10 @@
 const state = {
-  gallery: [{
-    id: 5,
-    default: '',
-    status: 'creating',
-    children:[{
-      status: 'creating',
-    },{
-      status: 'await',
-    },{
-      status: 'await',
-    },{
-      status: 'await',
-    }]
-  }],
-  underWay:{
-    id: 5,
-    default: '',
-    status: 'creating',
-    children:[{
-      status: 'creating',
-    },{
-      status: 'await',
-    },{
-      status: 'await',
-    },{
-      status: 'await',
-    }]
-  }
+  user: null
 }
 
 const mutations = {
-  SET_CONFIG(state,list){
-    state.gallery = list
-  },
-  UP_UNDERWAY(state,{ key,value }){
-    state.underWay[key] = value;
-  },
-  UP_UNDERWAY_CHILD(state,{ index, key, value }){
-    state.underWay.children[index][key] = value;
-  },
-  SET_UNDERWAY(state, info) {
-    state.underWay = info;
+  SET_USER(state,info){
+    state.user = info
   }
 }
 
